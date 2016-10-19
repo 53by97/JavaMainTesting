76feba9cdb49c3d983a4c82ca4194b4a5fe44796
53by97 <srimanta_sahu@yahoo.com>
2016-10-19 14:52:38 +0530

    Initial Commit

---
+++ src/main/java/com/kvvssut/learnings/java/loops/SwitchLoop.java
@@ -0,0 +1,21 @@
+package com.kvvssut.learnings.java.loops;
+
+public class SwitchLoop {
+
+	public static void main(String[] args) {
+		// expressionKeyAsNull();
+	}
+
+	private static void expressionKeyAsNull() {
+		Integer key = null;
+		switch (key) {
+		case 1:
+			break;
+		case 2:
+			break;
+		default:
+			break;
+		}
+	}
+
+}
---
+++ src/main/java/com/kvvssut/learnings/java/operators/IncrementOperators.java
@@ -0,0 +1,14 @@
+package com.kvvssut.learnings.java.operators;
+
+public class IncrementOperators {
+
+	public static void main(String[] args) {
+		// postprefixAddition(3, 5);
+
+	}
+
+	private static void postprefixAddition(int a, int b) {
+		System.out.println(a+++b + " " + a + " " + b);
+	}
+
+}
