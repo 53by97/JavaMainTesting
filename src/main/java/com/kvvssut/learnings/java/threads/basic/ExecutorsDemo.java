df50df05955b238ea7b554dcd2817496812ef0ef
53by97 <srimant.sahu@gmail.com>
2016-11-28 08:33:52 +0530

    Added SQL DDL/DML/DQL queries
    Also added ExecutorService & ThreadPoolExecutor diff

--- src/main/java/com/kvvssut/learnings/java/threads/basic/ExecutorsDemo.java
+++ src/main/java/com/kvvssut/learnings/java/threads/basic/ExecutorsDemo.java
@@ -1,31 +1,45 @@
 package com.kvvssut.learnings.java.threads.basic;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-import com.kvvssut.learnings.java.threads.basic.ConsumerRunnable;
+class NewTask implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " - Measuring the end to end latency of client code");
+	}
+}
 
 public class ExecutorsDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		ConsumerRunnable runnable = new ConsumerRunnable();
+		exectuingMultipleThreadsWithExecutorService();
 
-		// ExecutorService executor = Executors.newSingleThreadExecutor();
-		ExecutorService executor = Executors.newFixedThreadPool(10);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
-		executor.submit(runnable);
+		exectuingMultipleThreadsWithThreadPoolExecutor();
+	}
 
+	private static void exectuingMultipleThreadsWithExecutorService() throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(20);	// internally returns new ThreadPoolExecutor(20, 20, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+		for (int i = 0; i < 100; i++) {
+			executor.submit(new NewTask());
+		}
 		executor.shutdown();
+		executor.awaitTermination(1l, TimeUnit.HOURS);
+	}
 
+	private static void exectuingMultipleThreadsWithThreadPoolExecutor() {
+		BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<Runnable>(100);	// with ThreadPoolExecutor we can choose our implementation of the blocking queue and specify the keepAliveTime as convenient
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(20, 50, 1l, TimeUnit.MINUTES, blockingQueue);
+		threadPoolExecutor.prestartAllCoreThreads();
+		for (int i = 0; i < 100; i++) {
+			threadPoolExecutor.execute(new NewTask());
+		}
 	}
 
 }
