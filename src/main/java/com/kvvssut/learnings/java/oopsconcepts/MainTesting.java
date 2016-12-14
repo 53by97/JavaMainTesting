f28b176f4baf310ce64cd12a4a60167cc359eefa
53by97 <srimanta_sahu@yahoo.com>
2016-12-14 11:18:32 +0530

    Added utilitity methods for date and time

--- src/main/java/com/kvvssut/learnings/java/oopsconcepts/MainTesting.java
+++ src/main/java/com/kvvssut/learnings/java/oopsconcepts/MainTesting.java
@@ -4,7 +4,19 @@ public class MainTesting {
 
 	public static void main(String[] args) {
 		System.out.println(-10 % -3); // -1
+		
+		System.out.println("sri:ma.nt:sa:hu.j".replaceAll("[:.]", "-"));
+		
+		method(1);
+		
 
 	}
-
+	
+	public static void method(int i, String... args) {
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+		
+		System.out.println("No errors");
+	}
 }
