3d60026fccf7e325300842c6fd802bdb861b1b14
53by97 <srimant.sahu@gmail.com>
2016-12-17 18:52:14 +0530

    Added log2 of int in main testing

--- src/main/java/com/kvvssut/learnings/java/oopsconcepts/MainTesting.java
+++ src/main/java/com/kvvssut/learnings/java/oopsconcepts/MainTesting.java
@@ -10,6 +10,16 @@ public class MainTesting {
 		method(1);
 		
 
+		System.out.println(getLog2OfArrayLength(10));
+	}
+	
+	public static int getLog2OfArrayLength(int length) {
+		int log2OfLength = 0;
+		while (length > 0) {
+			log2OfLength++;
+			length /= 2;
+		}
+		return log2OfLength;
 	}
 	
 	public static void method(int i, String... args) {
